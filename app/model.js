70d80c132c51ede420adec9fe6565e0215587b5e
zaydee8223 <83433819+zaydee8223@users.noreply.github.com>
2022-10-09 13:54:19 -0400

    finishing books.html

--- app/model.js
+++ app/model.js
@@ -44,8 +44,40 @@ var bookListTwo = [ // this is the booklist for the second section
 var bookListThree = [ // this is the booklist for the third section
     {
         bookInfo: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Netus et malesuada fames ac.",
-        bookPrice: "$27.99",
-         bookImg: "books/finding-me.jpg",
+        bookPrice: "$19.99",
+         bookImg: "books/misery.jpg",
+    },
+
+    {
+        bookInfo: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Netus et malesuada fames ac.",
+        bookPrice: "$15.99",
+         bookImg: "books/frankenstein.jpg",
+    },
+
+    {
+        bookInfo: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Netus et malesuada fames ac.",
+        bookPrice: "$19.99",
+         bookImg: "books/phantoms.jpg",
+    },
+]
+
+var bookListFour = [ // this is the booklist for the fourth section
+    {
+        bookInfo: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Netus et malesuada fames ac.",
+        bookPrice: "$19.99",
+         bookImg: "books/winnie-the-pooh.jpg",
+    },
+
+    {
+        bookInfo: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Netus et malesuada fames ac.",
+        bookPrice: "$15.99",
+         bookImg: "books/cat-and-the-hat.jpg",
+    },
+
+    {
+        bookInfo: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Netus et malesuada fames ac.",
+        bookPrice: "$7.99",
+         bookImg: "books/fun-facts-about-space.jpg",
     },
 ]
 export function changePage(pageID, callback) {
@@ -60,7 +92,8 @@ export function changePage(pageID, callback) {
         $.get(`pages/${pageID}.html`, function (data){
             // console.log("data " + data);
             $("#app").html(data);
-            $.each(bookList,  function(idx, book){
+            //this is what will inject when someone is in the books page 
+            $.each(bookList,  function(idx, book){ // first set of books
                 $(".bookPages").append(`<div class="book">
                 <div class="bookImage">
                   <img src="images/${book.bookImg}" alt="" />
@@ -73,8 +106,34 @@ export function changePage(pageID, callback) {
               </div>
             </div>`);
             })
-            $.each(bookListTwo,  function(idx, book){
+            $.each(bookListTwo,  function(idx, book){ // second set of books
                 $(".bookPagesTwo").append(`<div class="book">
+                <div class="bookImage">
+                  <img src="images/${book.bookImg}" alt="" />
+                </div>
+                <div class="bookInfo">
+                  <p class = "infoText">${book.bookInfo}</p>
+                  <p class = "price">${book.bookPrice}</p>
+                  <button id="${idx}">ADD TO CART</button>
+                </div>
+              </div>
+            </div>`);
+            })
+            $.each(bookListThree,  function(idx, book){ // third set of books
+                $(".bookPagesThree").append(`<div class="book">
+                <div class="bookImage">
+                  <img src="images/${book.bookImg}" alt="" />
+                </div>
+                <div class="bookInfo">
+                  <p class = "infoText">${book.bookInfo}</p>
+                  <p class = "price">${book.bookPrice}</p>
+                  <button id="${idx}">ADD TO CART</button>
+                </div>
+              </div>
+            </div>`);
+            })
+            $.each(bookListFour,  function(idx, book){ // fourth set of books
+                $(".bookPagesFour").append(`<div class="book">
                 <div class="bookImage">
                   <img src="images/${book.bookImg}" alt="" />
                 </div>
